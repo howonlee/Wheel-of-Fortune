2e47f4bd9a1d3bc409be6d88f376dda97183af15
hlee.howon@gmail.com <hlee.howon@gmail.com>
2012-02-02 18:11:05 -0800

    Bug fixing

--- wheeloffortune.py
+++ wheeloffortune.py
@@ -3,31 +3,52 @@ import random #this allows you to use functions in the 'random' module
 
 def init_words():
     words = []
-    with open("listofwords.txt") as file:
-        for line in file:
-            words.append(line)
+    words_file = open('listofwords.txt', 'r')
+    for line in words_file:
+        words.append(line.strip())
+    words_file.close()
+    return words
 
 def spin_wheel(top_cash):
     if (top_cash > 0):
-        return randint(1, top_cash)
+        return random.randint(1, top_cash)
     else:
         print "You must have a max cash amount of more than 0 dollars!"
         return
 
 def pick_word(player_word, correct_word):
-    if (lower(player_word) == lower(correct_word)):
+    if (player_word.lower() == correct_word.lower()):
         print "You got it right! " + player_word + " is the right word!"
         return True
     else:
         print "Sorry, " + player_word + " is not the right word"
         return False#eg: did they get it right
 
+def end_game(player_cash):
+    print "You have won $" + str(player_cash)
+    print "Thank you for playing!"
+
 def play():
     max_cash = 200
     words = init_words()
-    chosen_word = words[randint(0, len(words) - 1)]
     strikes = 0;
+    player_cash = 0;
     while (strikes < 4):
-        spin_wheel(max_cash)
+        poss_cash = spin_wheel(max_cash)
+        print "You have $" + str(player_cash)
+        print "You're playing for $" + str(poss_cash)
+        chosen_word = words[random.randint(0, len(words) - 1)]
+        print chosen_word
         player_input = raw_input('Enter your word: ')
-        pick_word(player_input, chosen_word)
+        if (player_input == "quit"):
+            end_game(player_cash)
+            break
+        has_won = pick_word(player_input, chosen_word)
+        if has_won:
+            player_cash += poss_cash
+        else:
+            strikes += 1
+    else :
+        end_game(player_cash)
+
+play()
